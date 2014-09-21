33907f150fa6daab2d8cc48bca5d8703640598a3
Vyacheslav Kozyrev <vyacheslav_kozyrev@epam.com>
2014-09-20 21:43:30 -0500

    const.js fix

--- ui-prototype/app/js/const.js
+++ ui-prototype/app/js/const.js
@@ -5,5 +5,5 @@ SERVICE_EMPLOYEE_SEARCH_URL = 'http://qst2014.herokuapp.com/api/employees';
 SERVICE_EMPLOYEE_INFO_URL = 'http://qst2014.herokuapp.com/api/employees/';
 SERVICE_EMPLOYEE_POSITIONS_URL = 'http://qst2014.herokuapp.com/api/positions';
 
-SERVICE_POSITION_INFO_URL = '/app/js/data/positionInfo.json';
-SERVICE_POSITION_EMPLOYEES_URL = '/app/js/data/positionEmployees.json';
+SERVICE_POSITION_INFO_URL = 'http://qst2014.herokuapp.com/js/data/positionInfo.json';
+SERVICE_POSITION_EMPLOYEES_URL = 'http://qst2014.herokuapp.com/js/data/positionEmployees.json';
